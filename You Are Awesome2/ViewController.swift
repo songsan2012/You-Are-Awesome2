d5e20cf496a8ede1b200b43d9354a0a11b9b4fdf
Song San <songsan2012@gmail.com>
2021-12-24 08:06:19 -0800

    Add images and image view

--- You Are Awesome2/ViewController.swift
+++ You Are Awesome2/ViewController.swift
@@ -13,27 +13,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var messageButtonPressed: UIButton!
       
+    @IBOutlet weak var messageImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("😎 The program just loaded now.")
+//        print("😎 The program just loaded now.")
         
-        messageLabel.text = "Fabulous?, that's you"
+//        messageLabel.text = "Fabulous?, that's you"
         
     }
     
     @IBAction func ShowMessage(_ sender: Any) {
-        print("🤣 The messsage button was pressed")
+//        print("🤣 The messsage button was pressed")
     
         messageLabel.text = "You Are Awesome!!!"
-        
-        print("DONE!")
+
+        messageImage.image = UIImage(named: "image0")
     
     }
 
     @IBAction func ShowAnotherMessage(_ sender: Any) {
         
         messageLabel.text = "You Are Great!!!!"
+        messageLabel.textColor = .blue
+        
+        messageImage.image = UIImage(named: "image1")
+        
         
     }
     
